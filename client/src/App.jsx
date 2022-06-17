7071fa16beea8716e8503bef6f5bc31b256e91f7
alexey432 <alexorl432@gmail.com>
2022-06-16 20:28:44 -0700

    P2-131 Routing is set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import './styles/index.scss'
-import Footer from './components/Footer';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div>
-      App
-        <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />}>
+          <Route path='protocols'>
+            <Route path="description" element={<div>description</div>} />
+            <Route path="guidelines" element={<div>guidelines</div>} />
+            <Route path="materials" element={<div>materials</div>} />
+            <Route path="steps" element={<div>steps</div>} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
