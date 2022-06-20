6083ef1074f2e2c0438fedcc4ee732c978e155b9
Debora Correia <deborapcorreia@gmail.com>
2022-06-19 18:23:03 -0700

    P2-96 Create components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,10 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import Description from './components/Dashboard/Protocols/Description';
+import Guidelines from './components/Dashboard/Protocols/Guidelines';
+import Materials from './components/Dashboard/Protocols/Materials';
+import Steps from './components/Dashboard/Protocols/Steps';
 
 import Dashboard from './components/Dashboard';
 import Protocols from './components/Dashboard/Protocols';
@@ -14,10 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />}>
           <Route path='protocols' element={<Protocols />}>
-            <Route path="description" element={<div>description</div>} />
-            <Route path="guidelines" element={<div>guidelines</div>} />
-            <Route path="materials" element={<div>materials</div>} />
-            <Route path="steps" element={<div>steps</div>} />
+            <Route path="description" element={<Description />} />
+            <Route path="guidelines" element={<Guidelines />} />
+            <Route path="materials" element={<Materials />} />
+            <Route path="steps" element={<Steps />} />
           </Route>
         </Route>
       </Routes>
--- client/src/components/Dashboard/Protocols/index.jsx
+++ client/src/components/Dashboard/Protocols/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Protocols = () => {
+    const [data, setData] = useState({
+        description: '',
+        guidelines: '',
+        materials: ''
+    });
+
     return (
         <div >
             Protocols
