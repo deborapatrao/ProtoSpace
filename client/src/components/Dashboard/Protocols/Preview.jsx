806b6b6c5828a012cb75b775c41986c179d4faf3
Siuly <siulygt@gmail.com>
2022-06-28 10:24:55 -0700

    changed the br tags to div, and  changing the main div to section

--- client/src/components/Dashboard/Protocols/Preview.jsx
+++ client/src/components/Dashboard/Protocols/Preview.jsx
@@ -7,60 +7,51 @@ const Preview = () => {
     const { data, handleDataChange } = useOutletContext();
 
     return (
-        <div>
+        <section>
+                
             <div className={'sectionTitle'}>
                 <h2>Description</h2>
             </div>
-            <label>Protocol Name</label>
-            <br/>
-            <p type={'text'}>{data.protocol}</p>
-            <br/>
-            <label>Abstract</label>
-            <br/>
-            <p type={'text'}>{data.abstract}</p>
-            <br/>
-            <label>Author</label>
-            <br/>
-            <p type={'text'}>{data.author}</p>
-            <br/>
-            <label>Disclaimer</label>
-            <br/>
-            <p type={'text'}>{data.disclaimer}</p>
-            <br/>
+            <div className={'description'}>
+                        <label>Protocol Name</label>
+                        <p type={'text'}>{data.protocol}</p>
+                        <label>Abstract</label>
+                        <p type={'text'}>{data.abstract}</p>
+                        <label>Author</label>
+                        <p type={'text'}>{data.author}</p>
+                        <label>Disclaimer</label>
+                        <p type={'text'}>{data.disclaimer}</p>
+            </div>
+          
             <div className={'sectionTitle'}>
                 <h2>Guidelines</h2>
             </div>
-            <label>Guidelines</label>
-            <br/>
-            <p type={'text'} >{data.guideline}</p>
-            <br/>
-            <label>Before start</label>
-            <br/>
-            <p type={'text'} >{data.before_start}</p>
-            <br/>
-            <label>Safety warnings</label>
-            <br/>
-            <p type={'text'} >{data.safety_warning}</p>
-            <br/>
+            <div className={'guidelines'}>
+                        <label>Guidelines</label>
+                        <p type={'text'}>{data.guideline}</p>
+                        <label>Before start</label>
+                        <p type={'text'}>{data.before_start}</p>
+                        <label>Safety warnings</label>
+                        <p type={'text'}>{data.safety_warning}</p>
             <label>
                 Confidentially policy
             </label>
-            <br/>
             <span>Sensitive content warning</span>
-            <br/>
             <span>Confidential</span>
-            <br/>
-            <div>
+            </div>
+
+            <div className={'sectionTitle'}>
                 <h2>Material</h2>
             </div>
-            <label>List of materials</label>
-            <br/>
-            <p type={'text'}>{data.materials}</p>
-            <br/>
+            <div className={'materials'}>
+                        <label>List of materials</label>
+                        <p type={'text'}>{data.materials}</p>
+            </div>
+
 
             <Link to={-1}>Back</Link>
 
-        </div>
+        </section>
     );
 }
 
