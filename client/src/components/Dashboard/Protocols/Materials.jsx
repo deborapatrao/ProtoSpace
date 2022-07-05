6c8cc91d8ca184f5e4d053f7875174d699767ad4
Siuly <siulygt@gmail.com>
2022-07-04 18:10:26 -0700

    Materials page styled

--- client/src/components/Dashboard/Protocols/Materials.jsx
+++ client/src/components/Dashboard/Protocols/Materials.jsx
@@ -4,23 +4,27 @@ import {
     useOutletContext
 } from "react-router-dom";
 import { TextField } from '@mui/material';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const Materials = () => {
     const { data, handleDataChange } = useOutletContext();
     return (
         <section>
-            <div>
+            <div className={"section-header"}>
+                <div className={"materials-title"}>
                 <h2>Material</h2>
-                <div><span className={"mandatory"}>*</span> This section need to be filled</div>
+                </div>
+                <div className={"mandatory-desc"}><span className={"mandatory"}>*</span> This section need to be filled</div>
             </div>
-            <div>
+            <div className={"materials-fields"}>
                 <label>List of materials<span className={"mandatory"}>*</span></label>
-                <TextField type={'text'} value={data.materials} onChange={(e) => handleDataChange(e.target.value, 'materials')} placeholder={'Materials'}/>
+                <TextField className={"materials-input"} type={'text'} value={data.materials} onChange={(e) => handleDataChange(e.target.value, 'materials')} placeholder={'Materials'}/>
             </div>
 
-            <div>
-                <Link to={"/protocols/guidelines"}>Guidelines</Link>
-                <Link to={"/protocols/steps"}>Steps</Link>
+            <div className={"navigation-links"}>
+                <div className={"link-guideline"}><ArrowBackIosNewIcon /><Link to={"/protocols/guidelines"}>Guidelines</Link></div>
+                <div className={"link-step"}><Link to={"/protocols/steps"}>Steps</Link><ArrowForwardIosIcon /></div>
             </div>
         </section>
     );
