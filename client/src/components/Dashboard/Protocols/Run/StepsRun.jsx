a5f1c1ee7c5c6ba42c17148c212da7146c9e4883
alexey432 <alexorl432@gmail.com>
2022-07-17 12:32:29 -0700

    P2-212 fix stepsRun post request parms

--- client/src/components/Dashboard/Protocols/Run/StepsRun.jsx
+++ client/src/components/Dashboard/Protocols/Run/StepsRun.jsx
@@ -19,7 +19,8 @@ const StepsRun = () => {
             const user = JSON.parse(localStorage.getItem('user'));
 
             const params = {
-                protocolId: protocolId
+                protocolId: protocolId,
+                workspace_id: user.workspaceId[0][0].workspaceId
             }
 
             try {
