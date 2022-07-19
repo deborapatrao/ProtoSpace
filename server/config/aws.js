a5f8eb13ac34511480ffe4cc40d4bc719ae85333
Gabriel Cordeiro <gpcordeiro@hotmail.com>
2022-07-18 21:49:32 -0700

    226 finishing step now also save notes of that steps

--- server/config/aws.js
+++ server/config/aws.js
@@ -1,15 +1,35 @@
 require('dotenv').config();
-const AWS = require("aws-sdk")
+const S3 = require("aws-sdk/clients/s3")
+const fs = require("fs");
+const util = require('util')
+const unlinkFile = util.promisify(fs.unlink)
 // const getImgBuffer = require("getImgBuffer")
-const bucketName = 'protospace-app'
-const {AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY} = process.env
 
-AWS.config.update({
+const {AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET_REGION, AWS_S3_BUCKET_NAME} = process.env
+
+
+const s3 = new S3({
     accessKeyId: AWS_ACCESS_KEY_ID,
     secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    region: 'us-west-1'
+    region: AWS_S3_BUCKET_REGION
 })
 
-const s3Bucket = new AWS.S3({params: {Bucket: bucketName}})
+/* The above code is uploading a file to AWS S3. */
+exports.profilePhoto = async (file, fileName) => {
+    let photoURL = ''
+    const fileStream = fs.createReadStream(file);
 
-module.exports = s3Bucket   
+    const uploadParams = {
+        Bucket: AWS_S3_BUCKET_NAME,
+        Body: fileStream,
+        Key: `profile/${fileName}.jpg`,
+        ContentType: 'image/jpg',
+    }
+
+    await s3.upload(uploadParams).promise().then(data => photoURL=data.Location)
+    await unlinkFile(file.path)
+
+    return photoURL
+}
+
+
