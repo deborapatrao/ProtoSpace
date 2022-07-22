f62b579270bae8a9ee9ee187119cc10e2d7c7a18
Debora Correia <deborapcorreia@gmail.com>
2022-07-21 22:05:54 -0700

    P2-235 add components to header, fix logo for screen sizes

--- client/src/pages/Header/Header.jsx
+++ client/src/pages/Header/Header.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import { Link } from 'react-router-dom';
 import PopUp from "../../components/PopUp/PopUp";
-import icon from "./Logo.png";
+import LogoMobile from "./logo-mobile.png";
+import LogoDesktop from "./logo-desktop.png";
 
+import { AppBar, Toolbar, Container, Hidden } from "@mui/material";
+import { Box } from "@mui/system";
 
 const HeaderNav = () => {
 
@@ -12,30 +15,57 @@ const HeaderNav = () => {
 
     return (<>
         {/* // <section className={"header-container"}> */}
-        <div className="site-identity">
-            <img src={icon} alt="logo" className="logo"/>
-            <h1 className="site-title">ProtoSpace</h1>
-        </div>
-        <nav>
-            <ul>
-                <li>
+        
+        {/* <div className="navigation-container"> */}
+            {/* <nav>
+                <ul>
+                    <li>
+                        <Link to={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={"/about"}>About</Link>
+                    </li>
+                    <li>
+                        <Link to={"/contact"}>Contact</Link>
+                    </li>
+                </ul>
+            </nav> */}
+
+            <AppBar position="sticky">
+            <Container>
+                <Toolbar>
+                    <Box>
+                    <div className="site-identity">
+                    <img src={LogoMobile} alt="logo" className="logo-mobile"/>
+                    <img src={LogoDesktop} alt="logo" className="logo-desktop"/>
+                    </div>
+                    </Box>
+
+                    <Hidden xsDown>
+                    <Box>
                     <Link to={"/"}>Home</Link>
-                </li>
-                <li>
                     <Link to={"/about"}>About</Link>
-                </li>
-                <li>
                     <Link to={"/contact"}>Contact</Link>
-                </li>
-                <li>
+                    </Box>
+                    </Hidden>
+
+                    <Box>
                     <button className={"demo-header"}><Link to={"/demo"}>Book a Demo</Link></button>
-                </li>
-                <li className={"login"}>
+                    </Box>
+
+                    <Box>
+                    <div className={"login"}>
                     <Avatar src="/broken-image.jpg" />
                     <PopUp />
-                </li>
-            </ul>
-        </nav>
+                    </div>
+                    </Box>
+
+                </Toolbar>
+                </Container>
+            </AppBar>
+
+        {/* </div> */}
+
 
         {/* // </section> */}
 
