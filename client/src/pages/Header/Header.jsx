82ef9029c6846aa31c8a567e73e9d79d39b01f78
Debora Correia <deborapcorreia@gmail.com>
2022-07-24 18:57:24 -0700

    P2-235 fix static pages styles

--- client/src/pages/Header/Header.jsx
+++ client/src/pages/Header/Header.jsx
@@ -1,16 +1,17 @@
 import "./header.scss";
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import { Link } from 'react-router-dom';
 import PopUp from "../../components/PopUp/PopUp";
 import LogoMobile from "./logo-mobile.png";
 import LogoDesktop from "./logo-desktop.png";
+import MenuIcon from "./menu.png";
 
-import { AppBar, Toolbar, Container, Hidden } from "@mui/material";
+import { AppBar, Toolbar, Container, IconButton, Drawer, List, ListItem } from "@mui/material";
 import { Box } from "@mui/system";
 
 const HeaderNav = () => {
-
+    const [open, setOpen] = useState(false)
 
 
     return (<>
@@ -41,27 +42,43 @@ const HeaderNav = () => {
                     </div>
                     </Box>
 
-                    <Hidden xsDown>
-                    <Box>
-                    <Link to={"/"}>Home</Link>
-                    <Link to={"/about"}>About</Link>
-                    <Link to={"/contact"}>Contact</Link>
-                    </Box>
-                    </Hidden>
+                    <div className="navigation-container">
+                        <Box sx={{ display: {xs:'none', md:'block'}}}
+                        className="navigation-links">
+                        <Link to={"/"}>Home</Link>
+                        <Link to={"/about"}>About</Link>
+                        <Link to={"/contact"}>Contact</Link>
+                        </Box>
 
-                    <Box>
-                    <button className={"demo-header"}><Link to={"/demo"}>Book a Demo</Link></button>
-                    </Box>
-
-                    <Box>
-                    <div className={"login"}>
-                    <Avatar src="/broken-image.jpg" />
-                    <PopUp />
+                        <Box>
+                        <button className={"demo-header"}><Link to={"/demo"}>Book a Demo</Link></button>
+                        </Box>
+                        <Box>
+                        <div className={"login"}>
+                        <Avatar src="/broken-image.jpg" />
+                        <PopUp />
+                        </div>
+                        </Box>
+                        <Box sx={{ display: {xs:'block', md:'none'}}}>
+                            <IconButton onClick={() => setOpen(true)}>
+                            <img src={MenuIcon}/>
+                            </IconButton>
+                        </Box>
                     </div>
-                    </Box>
 
                 </Toolbar>
                 </Container>
+
+                <Drawer anchor='right' open={open} 
+                    onOpen={() => setOpen(true)} 
+                    onClose={() => setOpen(false)}
+                    className="menu-drawer">
+                    <List>
+                        <ListItem><Link to={"/"}>Home</Link></ListItem>
+                        <ListItem><Link to={"/about"}>About</Link></ListItem>
+                        <ListItem><Link to={"/contact"}>Contact</Link></ListItem>
+                    </List>
+                </Drawer>
             </AppBar>
 
         {/* </div> */}
