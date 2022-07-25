ab5a90f5a5fd95545334f169052411daca17b8ea
Gabriel Cordeiro <gpcordeiro@hotmail.com>
2022-07-24 19:33:56 -0700

    P2-239 Creating export to save as PDF

---
+++ server/routes/exportsRoute.js
@@ -0,0 +1,12 @@
+const controller = require("../images/generatePDF");
+const {authJwt} = require("../middleware");
+
+let router = require("express").Router();
+
+
+module.exports = function (app) {
+
+    router.post("/pdf", controller.exportPDF);
+
+    app.use('/api/generates', router);
+};
