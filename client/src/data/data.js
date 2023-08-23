bcf42131a9fc5ef613d7e949c40b98a34cdb5379
deborapatrao <deborapcorreia@gmail.com>
2023-08-23 14:08:45 -0700

    railway hosting

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,11 +1,9 @@
 let HOST_URL = "http://localhost:8080";
 
 if (process.env.NODE_ENV === "production") {
-    HOST_URL = "https://www.gabriecordeiro.ca"
+  HOST_URL = "protospace-production.up.railway.app";
 }
 // export const HOST_URL = "https://www.gabriecordeiro.ca";
 // export const HOST_URL = "http://localhost:8080";
 
 export { HOST_URL };
-
-
